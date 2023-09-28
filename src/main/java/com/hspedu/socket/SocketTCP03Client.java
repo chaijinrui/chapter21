c566326b7163b07e1c7e839735f51b867d182941
sns-chaijinrui <chai0304@163.com>
2023-09-28 10:35:57 +0800

    11

--- src/main/java/com/hspedu/socket/SocketTCP03Client.java
+++ src/main/java/com/hspedu/socket/SocketTCP03Client.java
@@ -18,19 +18,24 @@ public class SocketTCP03Client {
     @Test
     public void testSocketTCP03Client() throws IOException {
         //        链接服务器--链接如下机器的9999端口，如果链接成功，返回Socket对象
-        Socket socket = new Socket(InetAddress.getLocalHost(), 9998);
+        Socket socket = new Socket(InetAddress.getLocalHost(), 9999);
         System.out.println("客户端连接成功");
 //        客户端发送数据给服务端
 //        得到和这个Socket对象关联的输出流对象
 //        socker独有的getoutputStream
+//        字节流转字符流转缓冲流
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         bufferedWriter.write("hello, 字符流服务端~");
+
 //        如果使用字符流，需要手动刷新，否则数据不会写入数据通道
         bufferedWriter.flush();
-//        插入一个换行符，表示写入的内容结束,,要求另一方readline方法
-        bufferedWriter.newLine();
+
+//        结束标记
+        socket.shutdownOutput();
+
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        System.out.println(bufferedReader.readLine());
+        System.out.println("收到的消息：" + bufferedReader.readLine());
 
         bufferedReader.close();
         bufferedWriter.close();
